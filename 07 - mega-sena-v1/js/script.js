aafcbf9f4407f367690f53f83ca5d0041acb871b
Erick Luciano <erickhbd@gmail.com>
2022-08-09 11:07:15 -0300

    finalização do jogo de Mega Sena

--- 07 - mega-sena-v1/js/script.js
+++ 07 - mega-sena-v1/js/script.js
@@ -1,10 +1,134 @@
 //var board = []
+
 //var currentGame = [1, 5, 11, 13, 15, 17];
 //var savedGames = []
 
 var state = {board: [], currentGame: [], savedGames: []};
 
 function start () {
+    createBoard();
+    newGame();
+}
+
+function createBoard () {
+    state.board = [];
+
+    for (var i = 1; i <= 60; i++) {
+        state.board.push(i);
+    }
+}
+
+function newGame () {
+    resetGame();
+    render();
+
+    console.log(state.currentGame);
+}
+
+function render () {
+    renderBoard ();
+    renderButtons();
+    renderSavedGames();
+}
+
+function renderBoard() {
+    var divBoard = document.querySelector('#megasena-board');
+    divBoard.innerHTML = '';
+
+    var ulNumbers = document.createElement('ul');
+    ulNumbers.classList.add('numbers');
+
+    for (var i = 0; i < state.board.length; i++) {
+        var currentNumber = state.board[i];
+
+        var liNumber = document.createElement('li');
+        liNumber.textContent = currentNumber;
+        liNumber.classList.add('number');
+
+        liNumber.addEventListener('click', handleNumberClick);
+
+            if (isNumberInGame(currentNumber)) {
+                liNumber.classList.add('selected-number')
+            }
+
+        ulNumbers.appendChild(liNumber);
+    }
+
+    divBoard.appendChild(ulNumbers);
+}
+
+function handleNumberClick(event) {
+    var value = Number(event.currentTarget.textContent);
+    
+    if(isNumberInGame(value)) {
+        removeNumberFromGame(value);
+    } else {
+        addNumberToGame(value);
+    }
+
+    console.log(state.currentGame);
+    render();
+}
+
+function renderButtons () {
+    var divButtons = document.querySelector('#megasena-buttons');
+    divButtons.innerHTML = '';
+
+    var buttonNewGame = createNewGameButton ();
+    var buttonRandomGame = createRandomGameButton();
+    var buttonSaveGame = createSaveGameButton();
+
+    divButtons.appendChild(buttonNewGame);
+    divButtons.appendChild(buttonRandomGame);
+    divButtons.appendChild(buttonSaveGame);
+}
+
+function createNewGameButton() {
+    var button = document.createElement('button');
+    button.textContent = 'Novo Jogo';
+
+    button.addEventListener('click', newGame);
+    return button;
+}
+
+function createRandomGameButton() {
+    var button = document.createElement('button');
+    button.textContent = 'Jogo Aleatório';
+
+    button.addEventListener('click', randomGame);
+    return button;
+}
+
+
+function createSaveGameButton() {
+    var button = document.createElement('button');
+    button.textContent = 'Salvar Jogo';
+    button.disabled = !isGameComplete();
+
+    button.addEventListener('click', saveGame);
+    return button;
+}
+
+function renderSavedGames () {
+     var divSavedGames = document.querySelector('#megasena-saved-games');
+     divSavedGames.innerHTML = '';
+
+     if (state.savedGames.length === 0) {
+        divSavedGames.innerHTML = '<p>Nenhum Jogo Salvo</p>';
+     } else {
+        var ulSavedGames = document.createElement('ul');
+
+        for (var i = 0; i < state.savedGames.length; i++) {
+            var currentGame = state.savedGames[i];
+
+            var liGame = document.createElement('li');
+            liGame.textContent = currentGame.join(', ');
+
+            ulSavedGames.appendChild(liGame);
+        }
+        divSavedGames.appendChild(ulSavedGames);
+     }
+}
     // addNumberToGame(1);
     // addNumberToGame(2);
     // addNumberToGame(3);
@@ -24,7 +148,6 @@ function start () {
 
     // resetGame();
     // console.log(state.currentGame);
-}
 
 function addNumberToGame(numberToAdd) {
     if (numberToAdd < 1 || numberToAdd > 60) {
@@ -74,10 +197,13 @@ function addNumberToGame(numberToAdd) {
 
     function saveGame () {
         if (!isGameComplete()) {
-            //console.error('O jogo não está completo');
+            console.error('O jogo não está completo');
             return;
         }
         state.savedGames.push(state.currentGame);
+        newGame();
+
+        console.log(state.savedGames)
     }
 
     function isGameComplete() {
@@ -86,6 +212,18 @@ function addNumberToGame(numberToAdd) {
 
     function resetGame () {
         state.currentGame = [];
+    }
+
+    function randomGame() {
+        resetGame();
+
+    while (!isGameComplete()){
+        var randomNumber = Math.ceil(Math.random() * 60);
+
+        addNumberToGame(randomNumber);
+       }
+       console.log(state.currentGame);
+       render();
     }
 
 start();
